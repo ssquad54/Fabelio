1cf4d684b02409be2591488f7777907cba498d43
ssquad54 <bladegangster@yahoo.co.id>
2020-09-24 02:21:01 +0700

    Fix beforeSubmit Shipping Count. IF: current + 1 & SO: Sum of all IF

--- Fabelio/beforeSubmitShippingCount.js
+++ Fabelio/beforeSubmitShippingCount.js
@@ -8,37 +8,136 @@
 define(['N/record', 'N/search', 'N/transaction'],
     function(record, search, transaction) {
         function beforeSubmit(context) {
-            if (context.type !== context.UserEventType.CREATE && context.type !== context.UserEventType.EDIT && context.type !== context.UserEventType.SHIP)
-                return;
+            var oldRecord;
+            var oldStatus;
+            var newRecord;
+            var newStatus;
 
-            var newRecord = context.newRecord;
-            log.debug("newRecord", newRecord);
+            // on Create 
+            if (context.type == context.UserEventType.CREATE) {
+                newRecord = context.newRecord;
+                newStatus = newRecord.getText('shipstatus');
+                log.debug("Shipping Status", newStatus)
 
-            var newShipStatus = newRecord.getText('shipstatus')
-            log.debug("newShipStatus", newShipStatus);
+                if (newStatus == 'Shipped') {
+                    newRecord.setValue({
+                        fieldId: 'custbody_kri_shipping',
+                        value: (1).toString()
+                    });
 
-            var shipCountSearch = search.create({
-                type: search.Type.TRANSACTION,
-                title: 'Shipping Try in Item Fulfillment',
-                id: 'customsearch_shipping_try_if',
-                columns: [
-                    search.createColumn({
-                        name: "newvalue",
-                        join: "systemNotes"
-                    })
-                ],
-                filters: [
-                    ["internalid", "is", newRecord.id],
-                    "AND", ["systemnotes.newvalue", "is", "Shipped"],
-                    "AND", ["mainline", "is", "T"]
-                ]
-            }).run().getRange(0, 500);
+                    var so = record.load({
+                        type: record.Type.SALES_ORDER,
+                        id: newRecord.getValue('createdfrom')
+                    });
 
-            log.debug("shipCountSearch.length", shipCountSearch.length);
+                    // Search Shipping Count from all Item Fulfillment related with Sales order
+                    var soShippingSearch = search.create({
+                        type: search.Type.ITEM_FULFILLMENT,
+                        title: 'Shipping Count for SO',
+                        id: 'customsearch_shipping_count_so',
+                        columns: [
+                            search.createColumn({
+                                name: "custbody_kri_shipping"
+                            })
+                        ],
+                        filters: [
+                            ["createdfrom", "is", newRecord.getValue('createdfrom')],
+                            "AND", ["mainline", "is", "T"]
+                        ]
+                    }).run().getRange(0, 500);
 
-            newRecord.setValue({
-                fieldId: 'custbody_shipping_count',
-                value: shipCountSearch.length + 1
-            });
+                    log.debug("soShippingSearch", soShippingSearch);
+
+                    var soShippingCount = 1;
+                    for (var i = 0; i < soShippingSearch.length; i++) {
+
+                        var IFshippingCount = soShippingSearch[i].getValue('custbody_kri_shipping')
+                        log.debug("IFshippingCount", IFshippingCount);
+
+                        soShippingCount += Number(soShippingSearch[i].getValue('custbody_kri_shipping'));
+                        log.debug("soShippingCount", soShippingCount);
+                    }
+                    // Populate Shipping Count to Sales Order
+                    var so = record.load({
+                        type: record.Type.SALES_ORDER,
+                        id: newRecord.getValue('createdfrom')
+                    });
+
+                    so.setValue({
+                        fieldId: 'custbody_kri_shipping',
+                        value: (soShippingCount).toString()
+                    });
+
+                    var saveSO = so.save()
+                    log.debug("internalid SO", saveSO);
+                };
+            } else if (context.type !== context.UserEventType.CREATE) {
+                oldRecord = context.oldRecord;
+                newRecord = context.newRecord;
+
+                newStatus = newRecord.getText('shipstatus')
+                log.debug("newStatus", newStatus);
+
+                oldStatus = oldRecord.getText('shipstatus');
+                log.debug("oldStatus", oldStatus);
+
+                if (oldStatus == 'Shipped')
+                    return;
+                else if (newStatus == 'Shipped') {
+                    var shippingCount = newRecord.getValue({
+                        fieldId: 'custbody_kri_shipping'
+                    });
+                    log.debug("shipping Count", shippingCount);
+
+                    newRecord.setValue({
+                        fieldId: 'custbody_kri_shipping',
+                        value: shippingCount + 1
+                    });
+
+                    // Search Shipping Count from all Item Fulfillment related with Sales order
+                    var soShippingSearch = search.create({
+                        type: search.Type.ITEM_FULFILLMENT,
+                        title: 'Shipping Count for SO',
+                        id: 'customsearch_shipping_count_so',
+                        columns: [
+                            search.createColumn({
+                                name: "custbody_kri_shipping"
+                            })
+                        ],
+                        filters: [
+                            ["createdfrom", "is", newRecord.getValue('createdfrom')],
+                            "AND", ["mainline", "is", "T"]
+                        ]
+                    }).run().getRange(0, 500);
+
+                    log.debug("soShippingSearch", soShippingSearch);
+
+                    var soShippingCount = 1;
+                    for (var i = 0; i < soShippingSearch.length; i++) {
+
+                        var IFshippingCount = soShippingSearch[i].getValue('custbody_kri_shipping')
+                        log.debug("IFshippingCount", IFshippingCount);
+
+                        soShippingCount += Number(soShippingSearch[i].getValue('custbody_kri_shipping'));
+                        log.debug("soShippingCount", soShippingCount);
+                    }
+                    // Populate Shipping Count to Sales Order
+                    var so = record.load({
+                        type: record.Type.SALES_ORDER,
+                        id: newRecord.getValue('createdfrom')
+                    });
+
+                    so.setValue({
+                        fieldId: 'custbody_kri_shipping',
+                        value: (soShippingCount).toString()
+                    });
+
+                    var saveSO = so.save()
+                    log.debug("internalid SO", saveSO);
+                }
+            }
         }
+        return {
+            beforeSubmit: beforeSubmit
+        };
     });
