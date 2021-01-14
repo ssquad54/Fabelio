9ed692b3aff79d1336f57c22660b0fb786d96dae
ssquad54 <bladegangster@yahoo.co.id>
2021-01-14 20:34:09 +0700

    New

--- Fabelio/mass_update_deposit_app.js
+++ Fabelio/mass_update_deposit_app.js
@@ -2,7 +2,7 @@
  *@NApiVersion 2.0
  *@NScriptType MassUpdateScript
  */
-define(['N/transaction', 'N/record'], function(transaction, record) {
+define(['N/transaction', 'N/record', 'N/format'], function(transaction, record, format) {
     function each(params) {
 
         var depApp = record.load({
@@ -10,15 +10,27 @@ define(['N/transaction', 'N/record'], function(transaction, record) {
             id: params.id
         });
 
+        var depAppNo = depApp.getValue('tranid');
+        var depAppDate = depApp.getText('trandate');
+        var depAppAmt = depApp.getValue('applied');
+
+        log.debug('Deposit Application Internal ID', params.id);
+        log.debug('Deposit Application Number', depAppNo);
+        log.debug('Deposit Application Date', depAppDate);
+        log.debug('Deposit Application Amount', depAppAmt);
+
+        var dateValidate = format.parse({
+            value: '2020-1-1',
+            type: format.Type.DATE
+        });
+        log.debug('dateValidate', dateValidate);
+
         var applyTo = depApp.getSublistValue({
             sublistId: 'apply',
             fieldId: 'type',
             line: 0
         });
-        log.debug({
-            title: 'Apply to',
-            details: applyTo
-        });
+        log.debug('Apply to', applyTo);
 
         if (applyTo == 'Invoice') {
             var invDate = depApp.getSublistValue({
@@ -26,22 +38,20 @@ define(['N/transaction', 'N/record'], function(transaction, record) {
                 fieldId: 'applydate',
                 line: 0
             });
-            log.debug({
-                title: 'Inv Date',
-                details: invDate
-            });
+            log.debug('Inv Date', invDate);
 
-            depApp.setValue({
-                fieldId: 'trandate',
-                value: invDate
-            });
+            if (invDate < dateValidate) {
+                depApp.setValue({
+                    fieldId: 'trandate',
+                    value: invDate
+                });
 
-            depApp.save();
+                depApp.save();
 
-            log.debug({
-                title: 'Success !',
-                details: 'Updated !'
-            });
+                log.debug('Success !', 'Updated ! | Inv Number : ' + depApp.getSublistValue('apply', 'refnum', 0) + ' | Inv Date : ' + depApp.getSublistText('apply', 'applydate', 0) + ' | App Int ID : ' + params.id + ' | App No : ' + depAppNo + ' | App Date : ' + depAppDate + ' | Amount : ' + depAppAmt);
+            } else {
+                log.debug('Failed !', 'Not Updated ! | Inv Number : ' + depApp.getSublistValue('apply', 'refnum', 0) + ' | Inv Date : ' + depApp.getSublistText('apply', 'applydate', 0) + ' | App Int ID : ' + params.id + ' | App No : ' + depAppNo + ' | App Date : ' + depAppDate + ' | Amount : ' + depAppAmt);
+            }
 
         } else if (applyTo == 'Customer Refund') {
             var refundDate = depApp.getSublistValue({
@@ -49,22 +59,20 @@ define(['N/transaction', 'N/record'], function(transaction, record) {
                 fieldId: 'applydate',
                 line: 0
             });
-            log.debug({
-                title: 'refund Date',
-                details: refundDate
-            });
+            log.debug('refund Date', refundDate);
 
-            depApp.setValue({
-                fieldId: 'trandate',
-                value: refundDate
-            });
+            if (refundDate < dateValidate) {
+                depApp.setValue({
+                    fieldId: 'trandate',
+                    value: refundDate
+                });
 
-            depApp.save();
+                depApp.save();
 
-            log.debug({
-                title: 'Success !',
-                details: 'Updated !'
-            });
+                log.debug('Success !', 'Updated ! | Refund Number : ' + depApp.getSublistValue('apply', 'refnum', 0) + ' | Refund Date : ' + depApp.getSublistText('apply', 'applydate', 0) + ' | App Int ID : ' + params.id + ' | App No : ' + depAppNo + ' | App Date : ' + depAppDate + ' | Amount : ' + depAppAmt);
+            } else {
+                log.debug('Failed !', 'Not Updated ! | Refund Number : ' + depApp.getSublistValue('apply', 'refnum', 0) + ' | Refund Date : ' + depApp.getSublistText('apply', 'applydate', 0) + ' | App Int ID : ' + params.id + ' | App No : ' + depAppNo + ' | App Date : ' + depAppDate + ' | Amount : ' + depAppAmt);
+            }
         }
     }
     return {
